9274b492a58e09a0b33e681db42fddbfadf9a9c3
Max Vivar-Maas <mvivarmaas@gmail.com>
2020-10-31 16:26:43 -0400

    Initial commit

---
+++ src/main/java/me/mvivarmaas/givememusic/GivememusicApplication.java
@@ -0,0 +1,13 @@
+package me.mvivarmaas.givememusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivememusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GivememusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mvivarmaas/givememusic/GivememusicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mvivarmaas.givememusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivememusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
