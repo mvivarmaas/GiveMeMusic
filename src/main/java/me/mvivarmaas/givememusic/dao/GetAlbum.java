8599a57e797451178ad505a98a6c079362c0a7b9
Max Vivar-Maas <mvivarmaas@gmail.com>
2020-11-01 11:30:35 -0500

    Add to GetAlbum

--- src/main/java/me/mvivarmaas/givememusic/dao/GetAlbum.java
+++ src/main/java/me/mvivarmaas/givememusic/dao/GetAlbum.java
@@ -1,2 +1,36 @@
-package me.mvivarmaas.givememusic.dao;public class GetAlbum {
+package me.mvivarmaas.givememusic.dao;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.model_objects.specification.Album;
+import com.wrapper.spotify.requests.data.albums.GetAlbumRequest;
+
+/**
+ * Use this class to get albums via the Spotify-Web-Api
+ */
+public class GetAlbum {
+    private String accessToken;;
+    private SpotifyApi api;
+    private String albumId;
+
+    public GetAlbum(String accessToken, String albumId) {
+        this.albumId = albumId;
+        this.accessToken = accessToken;
+        api = new SpotifyApi.Builder()
+                .setAccessToken(this.accessToken)
+                .build();
+
+    }
+
+    final GetAlbumRequest getAlbumRequest = api.getAlbum(albumId).build();
+
+    Album getAlbum_Sync() {
+        try {
+            final Album album = getAlbumRequest.execute();
+            return album;
+        } catch (Exception e) {
+            System.out.println("Something went wrong! " + e.getMessage());
+            return null;
+        }
+    }
+
 }
